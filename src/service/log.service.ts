3af551542974473fbb8b9c6f8af38ecd33de21c7
Ruchira <ruchira@coredevone.com>
2022-07-12 15:42:40 +0530

    implement log service for logging needs

--- src/service/log.service.ts
+++ src/service/log.service.ts
@@ -1,4 +1,17 @@
 /**
  * manage the log requirements
- * TODO
+ * using console logging for now.
+ * if later we need to change logging mechanism, we can do that by only updating this file
  */
+
+export function logError(data: string) {
+    console.error(data);
+}
+
+export function logInfo(data: string) {
+    console.info(data);
+}
+
+export function logDebugInfo(data: string) {
+    console.debug(data);
+}
