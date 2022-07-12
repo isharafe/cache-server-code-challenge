cc690b46581820f3fdd67044a11f1dbae9e80fa7
Ruchira <ruchira@coredevone.com>
2022-07-12 16:34:50 +0530

    cache service code implemented. this contains the cache retrive and store logics

--- src/service/cache.service.ts
+++ src/service/cache.service.ts
@@ -9,14 +9,70 @@ will not be used. A new random value will then be generated (just like cache mis
 TTL will be reset on every read/cache hit.
 
  */
+
+import { randomString } from "../common/util";
+import { ICache } from "../model/cache.db.model";
+import * as DBService from "./db.service";
+import * as LogService from "./log.service";
+
+/**
+ * There is a limit to cache store.
+ * how can we decide this limit
+ * 1). hard code a sensible value
+ *      -- easy to use
+ *      -- hard to change
+ * 2). save this value in db and change as necessary
+ *      -- need a ui or an endpoint to update this value
+ *      -- easy to manag cache limit
+ * 3). write a logic to automatically adjust the max size based on available resources
+ *      -- too much work
+ *      -- don't need to worry about max size, as system will automatically take care of it
+ *
+ * for this code assignment, I'll go with above option 1) as it is easy to implement
+ */
 const MAX_CACHE_ENTRIES = 100;
 
+const DEFAULT_TTL_SECONDS = 1000;
 
-export const set = (key: string, value: any, ttl?: number) => {
-    // TODO
+export const set = async (key: string, value: any, ttl?: number) : Promise<ICache> => {
+
+    const now: number = new Date().getDate();
+    ttl = ttl || DEFAULT_TTL_SECONDS;
+
+    const dbRecord = {
+        createdAt: new Date(),
+        expireAt: new Date(now + ttl),
+        key: key,
+        value: value,
+        ttl: ttl
+    };
+
+    return DBService.save(dbRecord);
 };
 
-export const get = (key: string) => {
-    // TODO
-    return "";
+export const get = async (key: string) : Promise<string> => {
+
+    const value = await DBService.read(key);
+    const now = new Date().getDate();
+    let retValue;
+    let expireDate = value?.expireAt;
+    if(value && expireDate && expireDate?.getDate() >= now) {
+        LogService.logInfo("Cache hit");
+        DBService.updateTTL(value, value.ttl || DEFAULT_TTL_SECONDS);
+        retValue = value.value;
+    } else {
+        LogService.logInfo("Cache miss");
+        const newValue = getLatestValue();
+        set(key, newValue, DEFAULT_TTL_SECONDS);
+        retValue = newValue;
+    }
+
+    return retValue;
 };
+
+/**
+ * use async function assuming the new value will be fetching from a network location
+ */
+async function getLatestValue() : Promise<string> {
+    return randomString();
+}
